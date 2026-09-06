9425ed25cf00c2a7c3c3ebd0675ad4a491cf9166
Anshul Bhilare <terminatoranshul@gmail.com>
2026-09-06 14:59:08 +0530

    Update CredentialVaultServiceTest.java

--- src/test/java/com/dbbackup/service/CredentialVaultServiceTest.java
+++ src/test/java/com/dbbackup/service/CredentialVaultServiceTest.java
@@ -32,6 +32,7 @@ public class CredentialVaultServiceTest {
     @Test
     public void testSaveAndGetProfile() throws Exception {
         
+        
         CredentialProfile profile = new CredentialProfile();
         profile.setProfileName("prod-mysql");
         profile.setDbmsType(DbmsType.MYSQL);
